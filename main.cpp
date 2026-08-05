e362099a00c99685d82d7a55a5cd09793740d02b
Muhammad Jaafar <muhammad23ja@gmail.com>
2026-08-06 00:47:09 +0300

    Initial upload of linear data structures

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void push(int sta[], int &p , int &si);
-void pop(int &p);
-void used_size_sta(int &p);
-void display_sta(int sta[] , int &p);
+void ins_que(int que[], int &f , int &r , int &si);
+void del_que(int &f , int &r );
+void used_size_que(int &f , int &r , int &si);
+void display_que(int que[] , int &f , int &r);
 
 int main()
 {
-    int p=-1 ,opt=0 , sizeofarr=0;
-    cout<<" Enter size of stack : ";
+    int f=-1 , r=-1 ,opt , sizeofarr=0;
+    cout<<" Enter size of queue : ";
     cin>>sizeofarr;
-    int sta[sizeofarr];
+    int que[sizeofarr];
 
     do{
 
-        cout<<" 1-Push element. \n";
-        cout<<" 2-Pop element. \n";
-        cout<<" 3-Size of stack. \n";
-        cout<<" 4-Display stack. \n";
+        cout<<" 1-Insert number. \n";
+        cout<<" 2-Delete number. \n";
+        cout<<" 3-Size of queue. \n";
+        cout<<" 4-Display queue. \n";
         cout<<" 5- End process . \n";
         cout<<" Enter your choice : ";
         cin>>opt;
@@ -26,10 +26,10 @@ int main()
 
         switch(opt){
 
-        case 1: push(sta,p,sizeofarr);break;
-        case 2: pop(p);break;
-        case 3: used_size_sta(p);break;
-        case 4: display_sta(sta,p);break;
+        case 1: ins_que(que,f,r,sizeofarr);break;
+        case 2: del_que(f,r);break;
+        case 3: used_size_que(f,r,sizeofarr);break;
+        case 4: display_que(que,f,r);break;
         case 5: cout<<" Ending process. \n";break;
         default: cout<<"_________Wrong input_________\n";
         }
@@ -37,50 +37,72 @@ int main()
     return 0;
 }
 
-void push(int sta[] , int &p , int &si){
+void ins_que(int que[] , int &f , int &r , int &si){
     int num;
-    if(p==si-1){
-    cout<<" Overflow \n";
-    }
 
+    if(r<0){
+        r+=1;
+        cout<<" enter number : ";
+        cin>>num;
+        que[r]=num;
+        cout<<endl;
+        f+=1;
+
+    }
+    else if(r==si-1){
+        cout<<" Queue size is full of elements.\n ";
+    }
     else {
-    cout<<" Enter value : ";
-    cin>>num;
-    cout<<endl;
-    p+=1;
-    sta[p]=num;
+        r+=1;
+        cout<<" enter number : ";
+        cin>>num;
+        que[r]=num;
+        cout<<endl;
     }
 }
 
-void pop(int &p){
-    if(p<0){
-        cout<<" Stack is empty.\n";
+void del_que(int &f , int &r){
+
+    if(f<0&&r<0){
+        cout<<" queue is empty.  \n";
     }
-    else{
-     p-=1;
+    else if(f>-1&&f==r){
+        f=-1;
+        r=-1;
+        cout<<" last element deleted successfully. \n";
+    }
+    else {
+        f+=1;
     }
 }
 
-void used_size_sta( int &p){
-    int counter=0;
-    if(p<0){
-        cout<<" Stack is empty .\n";
+void used_size_que( int &f , int &r , int &si){
+    if(f<0&&r<0){
+        cout<<" queue is empty.\n ";
+        cout<<" Full size of queue is : "<<si<<endl;
+        cout<<" Used size in queue is : "<<0<<endl;
+    }
+    else {
+
+    int counter=0 ;
+    for(int i=f ; i<=r ; i++){
+        counter++;
+    }
+    cout<<" Full size of queue is : "<<si<<endl;
+    cout<<" Used size in queue is : "<<counter<<endl;
+    }
+}
+
+void display_que(int que[] , int &f , int &r){
+    if(f<0&&r<0){
+        cout<<" Queue is empty. \n";
     }
     else{
-        for(int i=0 ; i<=p ; i++){
-            counter+=1;
+     for(int i=f ; i<=r ;i++){
+        cout<<que[i]<<" ";
+        if(i==10){
+            cout<<endl;
         }
-        cout<<" used size is : "<<counter<<endl;
-    }
-}
-
-void display_sta(int sta[] , int &p){
-    if(p<0){
-        cout<<" stack is empty. \n";
-    }
-    else{
-     for(int i=p ; i>=0 ; i--){
-        cout<<sta[i]<<endl;;
     }
     cout<<endl;
     }
